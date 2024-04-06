56f4deedb0f2faaafa28bbf97ad10c65949c1f92
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2024-04-06 15:12:20 +0100

    feat: Added day 8

---
+++ day_8/README.md
@@ -0,0 +1,3 @@
+## Project Overview  
+This project is an Affirmation generator that sends affirmations to just two individuals to brighten up their day.  
+It was built as the project for my eight day of 100 days of code and it made use of the concepts of if, elif and else statements, nested if statements, concatenation, variables and printing out the contents of the variables created.
---
+++ day_8/main.py
@@ -0,0 +1,43 @@
+print("DAILY AFFIRMATIONS ✨✨")
+print()
+# Collecting user information
+name = input("What is your name?: ")
+
+print()
+if name == "Ayo" or name == "ayo":
+  dayOfTheWeek = input("What day of the week is it?: ")
+  if dayOfTheWeek == "Monday":
+    print("Keep shining like the bright star you are 🥰🥰")
+  elif dayOfTheWeek == "Tuesday":
+    print("You're amazing")
+  elif dayOfTheWeek == "Wednesday":
+    print("4 days down, keep grinding")
+  elif dayOfTheWeek == "Thursday":
+    print("Weird day right, anyways I hate you 😉😜")
+  elif dayOfTheWeek == "Friday":
+    print("TGIF, enjoy the weekend")
+  elif dayOfTheWeek == "Saturday":
+    print("Sleep in, you deserve it!!")
+  elif dayOfTheWeek == "Sunday":
+    print("First day of the new week huh, have a great week moving forward.")
+
+elif name == "Joseph" or name == "joseph":
+  pet_name = input("What is your pet's name?: ")
+  dayOfTheWeek = input("What day of the week is it?: ")
+  if dayOfTheWeek == "Monday":
+    print(pet_name, "loves you 💓💓")
+  elif dayOfTheWeek == "Tuesday":
+    print("Isn't", pet_name, "so cute!!!")
+  elif dayOfTheWeek == "Wednesday":
+    print(pet_name, "promises to never leave you 🥹🥹")
+  elif dayOfTheWeek == "Thursday":
+    print(pet_name, "wishes you a great day ahead")
+  elif dayOfTheWeek == "Friday":
+    print(pet_name, "said you're the best pet owner when a survey was conducted, yes you 🫵")
+  elif dayOfTheWeek == "Saturday":
+    print("Sleep in,", pet_name, "wants to cuddle")
+  elif dayOfTheWeek == "Sunday":
+    print(name, "and", pet_name, ": Greatest duo everrr")
+
+else:
+  print("My apologies, I do not know you but I do wish and hope you have one of the greatest days ever.")
