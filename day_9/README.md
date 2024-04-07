ea86b3915a18dc64a1740d4c11ec73479ec5c6a5
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2024-04-07 17:08:33 +0100

    feat: Added day 9

---
+++ day_9/README.md
@@ -0,0 +1,4 @@
+## Project Overview  
+This project is a generation generator.  
+Users input the year they were born and a message is printed out informing them which generation they belong to alongside a tacky or dad joke message.  
+Concepts used while building the program are input and print statements, variables, int to cast strings to numbers and if, elif and else statements.  
---
+++ day_9/main.py
@@ -0,0 +1,19 @@
+print("GENERATION GENERATOR 👨‍🦰👱‍♀️")
+print()
+print("Find out your generation name by simply answering the question of the year you were born.")
+print()
+birthYear = int(input("What is your year of birth?: "))
+print()
+
+if birthYear >= 1925 and birthYear <= 1946:
+  print("Wow you're a traditionalist, you got any traditional things for me 🌚")
+elif birthYear >= 1947 and birthYear <= 1964:
+  print("Baby Boomers right. No offence but what's up with that name 😅")
+elif birthYear >= 1965 and birthYear <= 1981:
+  print("Generation X, did you guys form the X men 😅😜")
+elif birthYear >= 1982 and birthYear <= 1995:
+  print("Millenials!! I'm digging the name but I still can't bring myself to understand it, are you guys like a thousand years old 😅")
+elif birthYear >= 1996 and birthYear <= 2015:
+  print("Generation Z!! You guys have been falling my hand 🥹")
+else:
+  print("Sunmimasen, but I don't know what generation you belong to. Check back later.")
